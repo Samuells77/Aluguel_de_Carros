e4a2d2516c036ed8856341a2fc581483199d5677
Samuel <38147282+Samuells77@users.noreply.github.com>
2023-05-07 15:46:57 -0300

    Add files via upload

---
+++ Aluguel de Carros.py
@@ -0,0 +1,4 @@
+kmrodados = float(input("Quantos quilômetros você andou com o carro?"))
+dias = int(input("Quantos dias você ficou com o carro?"))
+preço = (60*dias)+(kmrodados*0.15)
+print("O custo do aluguel do carro é: R${}".format(preço))
